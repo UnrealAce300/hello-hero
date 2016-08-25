de99083b7c86c94efdad8daf4409ad0092927a70
Gautam <gautam.ganeshmal@appliedis.com>
2016-08-25 11:00:56 -0400

    Show error messages, Add Report, new image

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
       'bower_components/angular-mocks/angular-mocks.js',
        'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/angular-toastr/dist/angular-toastr.tpls.js',
       
 
       // our app code
